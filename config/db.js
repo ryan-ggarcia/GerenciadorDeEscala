7ae896f9fa0d5e1934eb06136401b69078feffdf
Suporte <168090412+ryan-ggarcia@users.noreply.github.com>
2026-08-18 16:09:32 -0300

    fix: reajustes

---
+++ config/db.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+const { Pool } = require('pg')
+
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+})
--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-const server = express()
-
-const port = 5000
-server.listen(port, ()=>{
-    console.log(`Server on-line in port: localhost:${port} `)
-})
