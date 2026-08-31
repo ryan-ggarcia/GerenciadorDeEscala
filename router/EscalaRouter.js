324f9376116cde8ccce8eee675ba0816f9c90f79
Suporte <168090412+ryan-ggarcia@users.noreply.github.com>
2026-08-31 15:57:50 -0300

    fix: middleware nas telas de escala

--- router/EscalaRouter.js
+++ router/EscalaRouter.js
@@ -1,21 +1,22 @@
 import express from 'express'
 import EscalaController from '../controller/EscalaController.js'
 import SorteadorController from '../controller/SorteadorController.js';
+import AuthMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router()
 
 router.get('/', EscalaController.view);
-router.get('/exportar', EscalaController.exportar);
-router.get('/limpar-antigas', EscalaController.limparAntigasContar);
-router.post('/limpar-antigas', EscalaController.limparAntigas);
-router.get('/sorteador', SorteadorController.view);
-router.post('/sorteador/sortear', SorteadorController.sortear);
-router.post('/sorteador/sortear-mes', SorteadorController.sortearMes);
-router.post('/sorteador/salvar', SorteadorController.salvar);
-router.get('/cadastrar', EscalaController.viewCreate);
-router.post('/cadastrar', EscalaController.create);
-router.post('/alterar', EscalaController.update)
-router.post('/deletar', EscalaController.deletar)
-router.get('/alterar/:id', EscalaController.viewEdit);
+router.get('/exportar', AuthMiddleware.adminlogin , EscalaController.exportar);
+router.get('/limpar-antigas',AuthMiddleware.adminlogin, EscalaController.limparAntigasContar);
+router.post('/limpar-antigas',AuthMiddleware.adminlogin, EscalaController.limparAntigas);
+router.get('/sorteador',AuthMiddleware.adminlogin, SorteadorController.view);
+router.post('/sorteador/sortear',AuthMiddleware.adminlogin, SorteadorController.sortear);
+router.post('/sorteador/sortear-mes',AuthMiddleware.adminlogin, SorteadorController.sortearMes);
+router.post('/sorteador/salvar',AuthMiddleware.adminlogin, SorteadorController.salvar);
+router.get('/cadastrar',AuthMiddleware.adminlogin, EscalaController.viewCreate);
+router.post('/cadastrar',AuthMiddleware.adminlogin, EscalaController.create);
+router.post('/alterar',AuthMiddleware.adminlogin, EscalaController.update)
+router.post('/deletar',AuthMiddleware.adminlogin, EscalaController.deletar)
+router.get('/alterar/:id',AuthMiddleware.adminlogin, EscalaController.viewEdit);
 
 export default router
